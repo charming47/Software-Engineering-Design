3c5aee6be37215e2a5e67c0b50f0b9ad9ef2fb09
zffustb <15600758699@163.com>
2019-05-18 15:30:14 +0800

    在Student.php中写了一个稳定的面向对象测试

--- server/personel/student/student.php
+++ server/personel/student/student.php
@@ -1,7 +1,15 @@
 <?php
-	class student{
-		private string stu_id;
+	class Student{
+		private $stuId='';
 		
-		public function set
+		public function setStuId($stuId){
+			$this->stuId=$stuId;
+		}
+		public function getStuId(){
+			return $this->stuId;
+		}
 	}
+	$stu1=new Student();
+	$stu1->setStuId("1111111");
+	echo $stu1->getStuId();
 	
