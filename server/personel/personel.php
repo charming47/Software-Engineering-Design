5b9c4532dbcfc5f5e3d49a77c00a8308f2a47fe4
zffustb <15600758699@163.com>
2019-05-21 20:36:43 +0800

    把Personel类里面的update方法拆分完了。

--- server/personel/personel.php
+++ server/personel/personel.php
@@ -3,6 +3,7 @@
 		private $name='';
 		private $password='';
 		private $email='';
+		private $phoneNumber='';
 		private $expertise='';
 		
 		public function getName(){
@@ -23,6 +24,12 @@
 		public function getEmail(){
 			return $this->email;
 		}
+		public function getPhoneNumber(){
+			return $this->phoneNumber;
+		}
+		public function setPhoneNumber($phoneNumber){
+			$this->phoneNumber=$phoneNumber;
+		}
 		public function setExpertise($expertise){
 			$this->expertise=$expertise;
 		}
@@ -32,9 +39,14 @@
 
 		public abstract function initSinglePersonel();
 		
-		public abstract function updatePassword();//更新数据表中的人员的名字。
 		public abstract function updateName();//更新数据表中的人员的名字。
-		public abstract function updateName();
-
+		public abstract function updateEmail();
+		//更新数据表中的人员的电话号码。
+		public abstract function updatePhoneNumber();
+		//更新数据表中的人员的专长。
+		public abstract function updateExpertise();
+		//更新数据表中的人员的密码。
+		public abstract function updatePassword();
+		
 	}
 	
