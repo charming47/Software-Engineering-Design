52819581d85b57534086017044373e09eaf1af31
zffustb <15600758699@163.com>
2019-05-28 14:59:13 +0800

    删除了TeacherController.php中不必要的注释掉的代码。

--- server/personel/teacher/TeacherController.php
+++ server/personel/teacher/TeacherController.php
@@ -8,24 +8,6 @@ class TeacherController{
 		$singleTeacher->setTeacherTopic();
 		//$topicsOfTeacher是教师所拥有的选题数组。
 		$topicsOfTeacher=$singleTeacher->getTopicArr();
-		// echo "在TeacherController的showTopicsOfTeacher中，";
-		// if($topicsOfTeacher==null){
-			// echo '$topicsOfTeacher为空。<br>';
-		// }
-		// else{
-			// echo '$topicsOfTeacher不为空。<br>';
-		// }
-		// foreach($topicsOfTeacher as $singleTopic) {
-            // echo "<td>";
-            // echo $singleTopic['name'];
-            // echo "</td>";
-            // echo "<td>";
-            // echo $singleTopic['background'];
-            // echo "</td>";
-            // echo "<td>";
-            // echo $singleTopic['requirement'];
-            // echo "</td>";
-        // }
 		$singleTeacherView=new TeacherView();
 		$singleTeacherView->displayTopicsOfTeacher($topicsOfTeacher);
 	}
