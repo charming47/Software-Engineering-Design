25762e26f0d3ba17bc3142274653e686165cf19e
zffustb <15600758699@163.com>
2019-05-28 15:32:54 +0800

    在config.js中加入了教师获得选题的接口。

--- config.js
+++ config.js
@@ -10,5 +10,8 @@ var jquery=host+"/lib/jquery-3.3.1.min.js"
 //登录接口
 var login=host+"/server/login/login.php";
 
+//教师获得选题信息的接口
+var teacherGetTopic=host+"/server/personel/teacher/TeacherGetTopicInt.php";
+
 //后端提供的提交论文接口的接口地址。
 //var submit_paper_url=host+"/submit_paper.php";
